b27c62af2a3a89d823f66e34f898abd222d1b0e8
calisthenicsGuy <radevradoslav12@gmail.com>
2022-05-21 23:52:04 +0300

    tasks added

---
+++ 10. Functional Programming-Exercises/P04.Find_Evens_or_Odds/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            int[] givenNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int startIndex = givenNumbers[0];
+            int endIndex = givenNumbers[1];
+
+            string command = Console.ReadLine();
+
+            Predicate<int> predicate;
+            switch (command)
+            {
+                case "even": predicate = IsEven;
+                    break;
+                case "odd": predicate = IsOdd;
+                    break;
+                default: return;
+                    break;
+            }
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                numbers.Add(i);
+            }
+
+            numbers = Where(numbers, predicate).ToList();
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        static bool IsEven(int number) => number % 2 == 0;
+        static bool IsOdd(int number) => number % 2 == 1;
+
+        static List<int> Where(List<int> list, Predicate<int> predicate)
+        {
+            var newList = new List<int>();
+
+            foreach (int number in list)
+            {
+                if (predicate(number))
+                {
+                    newList.Add(number);
+                }
+            }
+
+            return newList;
+        }
+    }
+}
---
+++ 10. Functional Programming-Exercises/P08.Custom_Comparator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P08.Custom_Comparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, bool> func = num => num % 2 == 0;
+            int[] evenNumbers = FindEvenOrOddNums(func, numbers);
+            Array.Sort(evenNumbers);
+
+            func = num => num % 2 != 0;
+            int[] oddNumbers = FindEvenOrOddNums(func, numbers);
+            Array.Sort(oddNumbers);
+
+            numbers = ReturnFinalArray(evenNumbers, oddNumbers).ToArray();
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        static int[] FindEvenOrOddNums(Func<int, bool> predicate, int[] array)
+        { 
+            var newList = new List<int>();
+
+            foreach (int num in array)
+            {
+                if (predicate(num))
+                {
+                    newList.Add(num);
+                }
+            }
+
+            return newList.ToArray();
+        }
+
+        static int[] ReturnFinalArray(int[] arr1, int[] arr2)
+        {
+            var newList = new List<int>();
+            foreach (int num in arr1)
+            {
+                newList.Add(num);
+            }
+            foreach (int num in arr2)
+            {
+                newList.Add(num);
+            }
+
+            return newList.ToArray();
+        }
+    }
+}
