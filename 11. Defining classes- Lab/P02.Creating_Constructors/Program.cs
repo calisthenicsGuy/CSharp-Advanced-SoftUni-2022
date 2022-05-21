f260dc40cd79147d4c297ea9859f957fa7d9db40
calisthenicsGuy <radevradoslav12@gmail.com>
2022-05-21 18:36:27 +0300

    tasks added

---
+++ 11. Defining classes- Lab/P02.Creating_Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DefiningClasses;
+
+namespace P02.Creating_Constructors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 11. Defining classes- Lab/P07.Raw_Data/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacture
+{
+    public class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo, Tires[] tires)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+            Tires = tires;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tires[] Tires { get; set; }
+    }
+}
