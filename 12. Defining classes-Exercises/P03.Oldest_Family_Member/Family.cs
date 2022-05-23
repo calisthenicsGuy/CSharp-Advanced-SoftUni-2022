fbcec01ae866628b4879fb8d49bc475b9723a69d
calisthenicsGuy <radevradoslav12@gmail.com>
2022-05-24 00:04:17 +0300

    tasks added

---
+++ 12. Defining classes-Exercises/P03.Oldest_Family_Member/Family.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMembers(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            //int ageOfOldestPerson = 0;
+            //Person person = null;
+
+            //foreach (Person tempPerson in People)
+            //{
+            //    if (tempPerson.Age > ageOfOldestPerson)
+            //    {
+            //        person = tempPerson;
+            //        ageOfOldestPerson = tempPerson.Age;
+            //    }
+            //}
+
+            //return person;
+            
+            Person person = People
+                .OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+
+            return person;
+        }
+
+        public List<Person> GetPeopleThan30()
+        { 
+            List<Person> people = People
+                .Where(x => x.Age > 30).ToList();
+
+            return people;
+        }
+    }
+}
---
+++ 12. Defining classes-Exercises/P05.Date_Modifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P05.Date_Modifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.CalculateDifference(firstDate, secondDate));
+        }
+    }
+}
