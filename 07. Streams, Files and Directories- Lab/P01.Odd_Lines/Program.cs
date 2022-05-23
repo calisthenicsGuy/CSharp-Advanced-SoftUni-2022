e8f443336ad07030ec31d27eb162b77985ed4d75
calisthenicsGuy <radevradoslav12@gmail.com>
2022-05-23 23:59:22 +0300

    tasks added

---
+++ 07. Streams, Files and Directories- Lab/P01.Odd_Lines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace P01.Odd_Lines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"..\..\..\input.txt"))
+            {
+                string line = reader.ReadLine();
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    int counterOfLines = 0;
+                    while (line != null)
+                    {
+                        if (counterOfLines % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counterOfLines++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 07. Streams, Files and Directories- Lab/P02.Line_Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace P02.Line_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"..\..\..\input.txt"))
+            {
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    int counter = 1;
+                    while (!String.IsNullOrEmpty(line))
+                    {
+                        writer.WriteLine($"{counter++}. {line}");
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
