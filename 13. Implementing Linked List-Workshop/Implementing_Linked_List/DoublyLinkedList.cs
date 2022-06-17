d5c9ef5621acd8ef8fe39e9a27749c73cd11d853
calisthenicsGuy <radevradoslav12@gmail.com>
2022-06-17 23:35:03 +0300

    workshop added

---
+++ 13. Implementing Linked List-Workshop/Implementing_Linked_List/DoublyLinkedList.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implementing_Linked_List
+{
+    public class DoublyLinkedList<T>
+    {
+        public int Count { get; private set; }
+        public bool IsEmpty => this.Count == 0;
+
+        public LinkedListNode Head { get; private set; }
+        public LinkedListNode Tail { get; private set; }
+
+
+        public void AddHead(T element)
+        {
+            if (IsEmpty)
+            {
+                this.Head = this.Tail = new LinkedListNode(element);
+            }
+            else if (!IsEmpty)
+            {
+                var newHead = new LinkedListNode(element);
+                var previousHead = this.Head;          
+                this.Head = newHead;
+                previousHead.Previous = this.Head;
+                this.Head.Next = previousHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddTail(T element)
+        {
+            if (IsEmpty)
+            {
+                this.Tail = this.Head = new LinkedListNode(element);
+            }
+            else if (!IsEmpty)
+            {
+                var previousTail = this.Tail;
+                var newTail = new LinkedListNode(element);
+                this.Tail = newTail;
+                this.Tail.Previous = previousTail;
+                previousTail.Next = this.Tail;
+            }
+
+            this.Count++;
+        }
+
+        public T RemoveHead()
+        {
+            if (IsEmpty)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                throw new InvalidOperationException("Linked List is empty! Cannot remove element from empty linked list!");
+            }
+
+            var removedHead = this.Head;
+            var newHead = removedHead.Next;
+
+            if (newHead == null)
+            {
+                this.Head = null;
+                this.Tail = null;
+            }
+            else if (newHead != null)
+            {
+                newHead.Previous = null;
+                removedHead.Next = null;
+                this.Head = newHead;
+            }
+
+            this.Count--;
+            return removedHead.Value;
+        }
+
+        public T RemoveTail()
+        {
+            if (IsEmpty)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                throw new InvalidOperationException("Linked List is empty! Cannot remove element from empty linked list!");
+            }
+
+            var removedTail = this.Tail;
+            var newTail = removedTail.Previous;
+
+            if (newTail == null)
+            {
+                this.Head = this.Tail = null;
+            }
+            else if (newTail != null)
+            {
+                removedTail.Previous = null;
+                newTail.Next = null;
+                this.Tail= newTail;
+            }
+
+            this.Count--;
+            return removedTail.Value;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currNode = this.Head;
+            while (currNode != null)
+            {
+                action(currNode.Value);
+                currNode = currNode.Next;
+            }
+        }
+
+        public List<T> ToList()
+        {
+            var list = new List<T>();
+            this.ForEach(x => list.Add(x));
+            return list;
+        }
+
+        public T[] ToArray()
+        {
+            var array = new T[this.Count];
+            int counter = 0;
+            this.ForEach(x => array[counter++] = x);
+            return array;
+        }
+
+        public class LinkedListNode
+        {
+            public LinkedListNode(T value)
+            {
+                this.Value = value;
+            }
+
+            public LinkedListNode Previous { get; set; }
+            public T Value { get; set; }
+            public LinkedListNode Next { get; set; }
+        }
+    }
+}
---
+++ 13. Implementing Linked List-Workshop/Implementing_Linked_List/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace Implementing_Linked_List
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var item = new ImplementedLinkedList(15);
+            //item.Previous = new ImplementedLinkedList(14);
+            //item.Next = new ImplementedLinkedList(16);
+
+            //Console.WriteLine(item.Previous.Value);
+            //Console.WriteLine(item.Value);
+            //Console.WriteLine(item.Next.Value);
+
+            //item.Previous.Previous = new ImplementedLinkedList(13);
+            //Console.WriteLine(item.Previous.Previous.Value);
+
+            //item.Next.Next = new ImplementedLinkedList(17);
+            //Console.WriteLine(item.Next.Next.Value);
+
+            //List<ImplementedLinkedList> linkedList = new List<ImplementedLinkedList>();
+            //linkedList.Add(item);
+
+
+            //var list = new DoublyLinkedList();
+            //list.AddFirst(3);
+            //list.AddFirst(2);
+            //list.AddLast(1);
+            //list.AddLast(4);
+
+            //list.AddFirst(0);
+            //list.AddLast(100);
+
+            //list.RemoveFirst();
+            //list.RemoveLast();
+
+            //list.AddLast(5);
+            //Console.WriteLine(list.Count);
+
+            //Console.WriteLine(string.Join(", ", list.ToArray()));
+
+            //list.ForEach(i => Console.Write($"{i} "));
+
+            
+            DoublyLinkedList<int> linkedList = new DoublyLinkedList<int>();
+            linkedList.AddHead(1);
+            linkedList.AddHead(2);
+            linkedList.AddTail(3);
+            Console.WriteLine(linkedList.Head.Value);
+            Console.WriteLine(linkedList.Tail.Previous.Value);
+            Console.WriteLine(linkedList.Tail.Value);
+            // 2, 1, 3
+            Console.WriteLine();
+            Console.WriteLine(linkedList.RemoveHead());
+            Console.WriteLine(linkedList.Head.Value);
+            // 1, 3
+            Console.WriteLine();
+            linkedList.AddTail(5);
+            // 1, 3, 5
+            Console.WriteLine(linkedList.RemoveTail());
+            Console.WriteLine(linkedList.Tail.Value);
+            // 1, 3
+            linkedList.ForEach(x => Console.WriteLine(x));
+            var array = linkedList.ToArray();
+        }
+    }
+}
